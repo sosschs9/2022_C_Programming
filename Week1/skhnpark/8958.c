3e1c82cd9a997fb2c5e57d4f2ad39edeb5fde924
SKHNPARK <104781252+SKHNPARK@users.noreply.github.com>
2022-05-10 18:04:43 +0900

    Create 8958.c

---
+++ Week1/skhnpark/8958.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	int n, score, count;
+	char ary[80];
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		score = 0;
+		count = 0;
+
+		scanf("%s", &ary);
+		for (int j = 0; j < strlen(ary); j++) {
+			if (ary[j] == 'O') {
+				count++;
+				score += count;
+			}
+			else
+				count = 0;
+		}
+		printf("%d\n", score);
+	}
+}
