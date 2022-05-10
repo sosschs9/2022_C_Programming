c80092d11eca20017b4fdbb440d62bc9d9f0532c
SKHNPARK <104781252+SKHNPARK@users.noreply.github.com>
2022-05-10 18:03:21 +0900

    Create 2798.c

---
+++ Week1/skhnpark/2798.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void)
+{
+	int n, m, test;
+	int max = 0;
+	char ary[100];
+
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &ary[i]);
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = i+1; j < n; j++) {
+			for (int k = j+1; k < n; k++) {
+				test = ary[i] + ary[j] + ary[k];
+				if (test > max && test <= m)
+					max = test;
+			}
+		}
+	}
+	printf("%d", max);
+}
