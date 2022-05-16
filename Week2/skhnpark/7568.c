270f8911a637b1c04764aaf0e5fbf23fc5487763
SKHNPARK <104781252+SKHNPARK@users.noreply.github.com>
2022-05-17 02:23:02 +0900

    Update 7568.c

--- Week2/skhnpark/7568.c
+++ Week2/skhnpark/7568.c
@@ -1,1 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
 
+#include <stdio.h>
+
+int main(void)
+{
+	int N;
+	int x[50], y[50];
+
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d %d", &x[i], &y[i]);
+	}
+
+	for (int i = 0; i < N; i++) {
+		int k = 0;
+		for (int j = 0; j < N; j++) {
+			if (x[i] < x[j] && y[i] < y[j])
+				k++;
+		}
+		printf("%d ", k+1);
+	}
+
+	return 0;
+}
