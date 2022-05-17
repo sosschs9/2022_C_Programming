cfcfc2f666b886a3f2783020fe1c01ffcf759b24
mrbackjjang <105232071+mrbackjjang@users.noreply.github.com>
2022-05-17 14:46:51 +0900

    Create doungchi.c

---
+++ Week2/back/doungchi.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int N;
+	int count = 0;
+	int w[50] = { 0 }, t[50] = { 0 };
+
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%d %d", &w[i], &t[i]);
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		count = 1;
+		for (int j = 0; j < N; j++)
+		{
+			if (w[i] < w[j] && t[i] < t[j])
+			{
+				count++;
+			}
+			printf("%d ", count);
+		}
+	}
+}
