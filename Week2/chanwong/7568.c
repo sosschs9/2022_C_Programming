d3701a981d54387afd6f4c427f0de2afdfc8aff4
ChanWooong <104729817+ChanWooong@users.noreply.github.com>
2022-05-17 18:37:27 +0900

    Update 7568.c

--- Week2/chanwong/7568.c
+++ Week2/chanwong/7568.c
@@ -3,19 +3,20 @@ int main(){
 	int n ;
 	int tall[101];
 	int weight[101];
-	int rank = 1;
+	int rank;
 	scanf("%d", &n);
 	for(int i = 0 ; i<n; i++){
 		scanf("%d %d",&tall[i], &weight[i]);
 	}
 	for(int i = 0; i<n; i++){
+                rank = 1;
 		for(int j = 0; j <n ; j++){
 			if(tall[j]>tall[i] && weight[j]>weight[i]){
 				rank ++;
 			}
 		}
 		printf("%d ",rank);
-		rank = 1;
+		
 	}
 	
 }
