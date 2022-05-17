a626b8b21808dfc18334c2df204eecfa3f509d5e
martin <104724182+highburyto@users.noreply.github.com>
2022-05-17 13:55:25 +0900

    Delete martin directory

--- martin/.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-	unsigned long long int get[2];
-	for (int i = 0; i < 2; i++) {
-		scanf("%llu", &get[i]);
-	}
-	printf("%llu\n", get[0] + get[1]);
-    return 0;
-}
