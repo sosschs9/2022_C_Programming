03e9883972e7074eeb3c5f679f399ea8a3776c77
martin <104724182+highburyto@users.noreply.github.com>
2022-05-17 15:51:29 +0900

    Create 2798.c

---
+++ Week1/Martin/2798.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+	int N, M, i, j, k, get[110], result = 0;
+	scanf("%d", &N);
+	scanf("%d", &M);
+	for (i = 0; i < N; i++) {
+		scanf("%d", &get[i]);
+	}
+	for (i = 0; i < N - 2; i++) {
+		for (j = i + 1; j < N - 1; j++) {
+			for (k = j + 1; k < N; k++) {
+				if (get[i] + get[j] + get[k] > result && get[i] + get[j] + get[k] <= M) {
+					result = get[i] + get[j] + get[k];
+				}
+			}
+		}
+	}
+	printf("%d\n", result);
+}
