df1be19efcf9cea060700e1adc40ec1c65f6e81c
SKHNPARK <104781252+SKHNPARK@users.noreply.github.com>
2022-05-23 23:46:47 +0900

    Create 10798.c

---
+++ Week3/skhnpark/10798.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#define LINE 5
+#define MAX 15
+
+int main(void)
+{
+	char ary[MAX][LINE];
+
+	for (int i = 0; i < LINE; i++) {
+		scanf("%s", ary[i]);
+	}
+
+	for (int i = 0; MAX; i++) {
+		for (int j = 0; j < LINE; j++) {
+			printf("%c", ary[j][i]);
+		}
+	}
+}
