309e94adebe5141690e2d6668f012104dfecbf50
SKHNPARK <104781252+SKHNPARK@users.noreply.github.com>
2022-05-23 23:47:14 +0900

    Create 11650.c

---
+++ Week3/skhnpark/11650.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void)
+{
+	int N;
+	int x[100], y[100], temp = 0;
+
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		scanf("%d %d", x[i], y[i]);
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N - 1; j++) {
+			if (x[j] > x[j + 1]) {
+				temp = x[j];
+				x[j] = x[j + 1];
+				x[j + 1] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N - 1; j++) {
+			if (y[j] > y[j + 1]) {
+				temp = y[j];
+				y[j] = y[j + 1];
+				y[j + 1] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		printf("%d %d\n", x[i], x[i]);
+	}
+}
