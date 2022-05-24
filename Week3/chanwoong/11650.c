1657fe053a3b7a779a2edfac5751a847e4cef497
ChanWooong <104729817+ChanWooong@users.noreply.github.com>
2022-05-24 17:46:57 +0900

    Create 11650.c

---
+++ Week3/chanwoong/11650.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+void sortCoordinate(int n, int x[], int y[]);
+
+int main() {
+	int n;
+	int x[100001],y[100001];
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &x[i], &y[i]);
+	}
+	sortCoordinate(n, x, y);
+	
+}
+
+void sortCoordinate(int n, int x[], int y[]) {
+	int tmp;
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (x[i] > x[j] || y[i] > y[j]) {
+				tmp = x[i];
+				x[i] = x[j];
+				x[j] = tmp;
+				tmp = y[i];
+				y[i] = y[j];
+				y[j] = tmp;
+			}	
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		printf("%d %d\n", x[i], y[i]);
+	}
+}
