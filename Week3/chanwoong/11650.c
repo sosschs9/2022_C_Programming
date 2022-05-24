69766fee81ba0b6181f61adfc8dea9b6356bf2f9
ChanWooong <104729817+ChanWooong@users.noreply.github.com>
2022-05-24 20:22:26 +0900

    Update 11650.c

--- Week3/chanwoong/11650.c
+++ Week3/chanwoong/11650.c
@@ -5,7 +5,7 @@ void sortCoordinate(int n, int x[], int y[]);
 
 int main() {
 	int n;
-	int x[100001],y[100001];
+	int x[100000],y[100000];
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++) {
 		scanf("%d %d", &x[i], &y[i]);
@@ -18,10 +18,14 @@ void sortCoordinate(int n, int x[], int y[]) {
 	int tmp;
 	for (int i = 0; i < n; i++) {
 		for (int j = i + 1; j < n; j++) {
-			if (x[i] > x[j] || y[i] > y[j]) {
+			if (x[i] > x[j]) {
 				tmp = x[i];
 				x[i] = x[j];
 				x[j] = tmp;
+				tmp = y[i];
+				y[i] = y[j];
+				y[j] = tmp;
+			}else if (x[i] == x[j] && y[i] > y[j]) {
 				tmp = y[i];
 				y[i] = y[j];
 				y[j] = tmp;
