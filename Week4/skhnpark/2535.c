eb0eba1d256c575d867c03fcd4a8c473c131dd2a
SKHNPARK <104781252+SKHNPARK@users.noreply.github.com>
2022-05-31 00:21:15 +0900

    Create 2535.c

---
+++ Week4/skhnpark/2535.c
@@ -0,0 +1,42 @@
+// 실패!!
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+typedef struct
+{
+	int country;
+	int student_id;
+	int score;
+	int limit;
+
+} information;
+
+
+int main(void)
+{
+	int N;
+	int cnt = 0, k = 0;
+	information info[100];
+
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
+		scanf("%d %d %d", &info[i].country, &info[i].student_id, &info[i].score);
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N - 1; j++) {
+			if (info[j].score < info[j + 1].score) {
+				information temp = info[j];
+				info[j] = info[j + 1];
+				info[j + 1] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		info[i].limit = 0;
+	}
+
+	return 0;
+}
