c80e2fc7dd9ce97f6a0662e88fa6a8f1a6e34308
martin <104724182+docgpomodoro@users.noreply.github.com>
2022-05-30 18:49:36 +0900

    Update 17478.c

--- Week4/pomodoro/17478.c
+++ Week4/pomodoro/17478.c
@@ -5,8 +5,8 @@
 #define story1 "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어."
 #define story2 "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지."
 #define story3 "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\""
-#define end "라고 답변하였지."
 #define answer "\"재귀함수는 자기 자신을 호출하는 함수라네\""
+#define end "라고 답변하였지."
 void conversation(int repeat, int n);
 int main() {
 	int repeat; scanf("%d", &repeat);
