2a07904ec2eda82659f896d4b9b0038c7bf14efb
SKHNPARK <104781252+SKHNPARK@users.noreply.github.com>
2022-05-31 00:21:54 +0900

    Create 17478.c

---
+++ Week4/skhnpark/17478.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+void recursive_f(N);
+
+void recursive_f(N)
+{
+
+}
+
+
+int main(void)
+{
+	int N;
+	scanf("%d", &N);
+
+	printf("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
+	recursive_f(N);
+
+	return 0;
+}
