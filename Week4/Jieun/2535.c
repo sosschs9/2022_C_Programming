1882e2c19f340b0d366be16b4b731600dbfce279
Jieunbakk <105230699+Jieunbakk@users.noreply.github.com>
2022-05-31 18:41:03 +0900

    Update 2535.c

--- Week4/Jieun/2535.c
+++ Week4/Jieun/2535.c
@@ -7,7 +7,6 @@ typedef struct {
 	int score;
 }info;
 
-void sWip(info a, int c, int d);
 
 int main(void)
 {
@@ -16,6 +15,7 @@ int main(void)
 
 	info infor[1000];
 	int count=3;
+	info tept;
 
 	for (int i = 0; i < n; i++) {
 		scanf("%d %d %d", &infor[i].nat, &infor[i].stnum, &infor[i].score);
@@ -24,18 +24,22 @@ int main(void)
 	for (int i = 0; i < n; i++) {
 		for (int j = i + 1; j < n; j++) {
 			if (infor[i].score < infor[j].score) {
-				sWip(infor, j, i);
+				tept = infor[j];
+				infor[j] = infor[i];
+				infor[i] = tept;
 			}
 		}
 	}
 
-	/*for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) {
 		printf("%d %d %d\n", infor[i].nat, infor[i].stnum, infor[i].score);
-	}*/ //정렬잘되었는지 확인
+	}
 
 	while (1) {
 		if (infor[0].nat == infor[1].nat == infor[2].nat) {
-			sWip(infor, 2, count);
+			tept = infor[2];
+			infor[2] = infor[count];
+			infor[count] = tept;
 			count++;
 		}
 		else break;
@@ -47,10 +51,4 @@ int main(void)
 
 	return 0;
 	
-}
-void sWip(info a[], int c, int d) {
-	info tept;
-	tept = a[c];
-	a[c] = a[d];
-	a[d] = tept;
 }
